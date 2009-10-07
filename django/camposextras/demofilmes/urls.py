21e965dbbd3c47c91dfdc300b8c93beb99dd606b
Luciano Ramalho <luciano@ramalho.org>
2009-10-07 04:34:43 -0300

    inicio da edicao completa; algum trabalho pela frente

--- django/camposextras/demofilmes/urls.py
+++ django/camposextras/demofilmes/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.list_detail import object_list, object_detail
 from django.views.generic.create_update import create_object, update_object, delete_object
 
 from demofilmes.models import Filme
-from demofilmes.views import cadastro
+from demofilmes.views import cadastro, editar_completo
 
 filmes = Filme.objects.all()
 
@@ -24,6 +24,8 @@ urlpatterns = patterns('',
     url(r'^edtit/(?P<object_id>\d+)/$', update_object, 
         {'model':Filme}, 
         name='demofilmes.edtit'),
+    url(r'^edcomp/(?P<filme_id>\d+)/$', editar_completo, 
+        name='demofilmes.edcomp'),
     url(r'^del/(?P<object_id>\d+)/$', delete_object, 
         {'model':Filme, 'post_delete_redirect':'/demo/'}, # TODO: como retirar esta URL hardcoded? reverse aqui não funciona...
         name='demofilmes.del'),
--- django/camposextras/demofilmes/views.py
+++ django/camposextras/demofilmes/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
+# from django.forms.models import inlineformset_factory
+
+
 from demofilmes.models import Filme, Credito
 
 class FilmeForm(ModelForm):
@@ -17,6 +20,8 @@ class CreditoForm(ModelForm):
         model = Credito
 
 CreditosFormSet = formset_factory(CreditoForm, extra=3)
+
+# CreditosFormSet = inlineformset_factory(Filme, Credito, extra=3)
 
 def cadastro(request):
     if request.method == 'POST':
@@ -35,3 +40,23 @@ def cadastro(request):
         forms_creditos = CreditosFormSet()
     forms = dict(form_filme=form_filme, forms_creditos=forms_creditos)
     return render_to_response('demofilmes/filme_cadastro.html', forms)
+    
+def editar_completo(request, filme_id):
+    filme = Filme.objects.get(id=filme_id)
+    if request.method == 'POST':
+        form_filme = FilmeForm(request.POST)
+        if form_filme.is_valid():
+            filme = Filme.objects.create(**form_filme.cleaned_data)
+            forms_creditos = CreditosFormSet(request.POST)
+            if forms_creditos.is_valid():
+                for cred in forms_creditos.cleaned_data:
+                    cred['filme'] = filme
+                    Credito.objects.create(**cred)
+            # TODO: mais um caso em que não consegui usar um reverse...
+            return HttpResponseRedirect('/demo/ver/%s/' % filme.id)
+    else:
+        form_filme = FilmeForm(initial=filme.fields)
+        forms_creditos = CreditosFormSet()
+    forms = dict(form_filme=form_filme, forms_creditos=forms_creditos)
+    return render_to_response('demofilmes/filme_cadastro.html', forms)
+
