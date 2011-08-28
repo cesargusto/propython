219caa11966420e5a061fd88ae6aa1e49b451c3a
Luciano Ramalho <luciano@ramalho.org>
2011-08-28 01:31:58 -0300

    exemplo borg

--- fundamentos/patterns/singleton.py
+++ fundamentos/patterns/singleton.py
@@ -6,8 +6,7 @@ class Ordinaria(object):
 class Singleton(object):
     def __new__(cls, *a, **k):
         if not hasattr(cls, '_inst'):
-            cls._inst = super(Singleton,
-                cls).__new__(cls, *a, **k)
+            cls._inst = super(Singleton, cls).__new__(cls, *a, **k)
         return cls._inst
 
 class Foo(Singleton): pass
