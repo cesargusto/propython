ec3b736fdfadf09fce957d66e08e6fecd069aea7
Luciano Ramalho <luciano@ramalho.org>
2011-09-05 21:15:20 -0300

    fixed typo: miliseconds -> milliseconds

--- fundamentos/dateops/ext_json_date.py
+++ fundamentos/dateops/ext_json_date.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 """
-Convert an ISO-8601 date string to the miliseconds from the epoch
+Convert an ISO-8601 date string to the milliseconds from the epoch
 integer required by the Extended JSON format used by MongoDB [1]
 
 [1] http://www.mongodb.org/display/DOCS/Mongo+Extended+JSON
 
 Usage::
 
-    >>> datestr2milis('2008-04-01T03:28:50.625999')
+    >>> datestr2millis('2008-04-01T03:28:50.625999')
     1207020530625
-    >>> datestr2milis('1970-01-01T00:00:01.000')
+    >>> datestr2millis('1970-01-01T00:00:01.000')
     1000
-    >>> datestr2milis('1970-01-01T00:00:12.345')
+    >>> datestr2millis('1970-01-01T00:00:12.345')
     12345
-    >>> datestr2milis('1970-01-01T00:00:59.999')
+    >>> datestr2millis('1970-01-01T00:00:59.999')
     59999
-    >>> datestr2milis('1970-01-01T00:01:00.000')
+    >>> datestr2millis('1970-01-01T00:01:00.000')
     60000
-    >>> datestr2milis('9999-12-31T23:59:59.9999')
+    >>> datestr2millis('9999-12-31T23:59:59.9999')
     253402300799999
 
 """
@@ -30,11 +30,11 @@ ISO_8601 = '%Y-%m-%dT%H:%M:%S.%f'
 
 # use of timedelta.total_seconds suggested by Allison Vollmann
 if hasattr(timedelta, 'total_seconds'): # for Python >= 2.7
-    def datestr2milis(dt_str):
+    def datestr2millis(dt_str):
         td = datetime.strptime(dt_str, ISO_8601) - datetime(1970, 1, 1)
         return int(td.total_seconds() * 1000)
 else: # for Python 2.6
-    def datestr2milis(dt_str):
+    def datestr2millis(dt_str):
         td = datetime.strptime(dt_str, ISO_8601) - datetime(1970, 1, 1)
         return int((td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 1000)
 
