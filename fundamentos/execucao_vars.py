bcfb2c08949aeb4f5127dcd3b297cd9026a71baa
Luciano Ramalho <luciano@ramalho.org>
2011-09-12 10:46:37 -0300

    testes com vars locais

--- fundamentos/execucao_vars.py
+++ fundamentos/execucao_vars.py
@@ -2,9 +2,11 @@
 
 a = 10
 b = 20
+c = 30
+d = 40
 
 def func_a():
-    print a #
+    print a # this is a reference to the global variable
     return 'result of func_a'
 
 def func_b():
@@ -14,10 +16,18 @@ def func_b():
     return 'result of func_b'
 
 def func_c():
-    print c # referencing 'b' here may cause UnboundLocalError
+    print c # referencing 'c' here causes UnboundLocalError
     if False:
-        c = 33 # this line is unreachable, but c is seen on the left side of an assignment
+        c = 33 # this line is unreachable, but the compiler sees c on the
+        # left side of an assignment, and treats it as a local variable
     return 'result of func_c'
+
+def func_d():
+    print d # this is a reference to the global variable
+    def inner():
+        d = 33 # this creates a variable local to inner
+    inner()
+    return 'result of func_d'
 
 def test_a():
     res_a = ''
@@ -37,14 +47,23 @@ def test_b():
 def test_c():
     res_c = ''
     try:
-        res_c = func_b()
+        res_c = func_c()
     except UnboundLocalError as e:
-        assert e.args[0] == "local variable 'b' referenced before assignment"
+        assert e.args[0] == "local variable 'c' referenced before assignment"
+    else:
+        assert False, 'an UnboundLocalError excepion was expected but not raised'
+    assert res_c == ''
+
+def test_d():
+    res_d = ''
+    res_d = func_d()
+    assert res_d == 'result of func_d'
 
 if __name__=='__main__':
     test_a()
     test_b()
     test_c()
+    test_d()
     '''
     import dis
     dis.dis(func_a.__code__.co_code)
