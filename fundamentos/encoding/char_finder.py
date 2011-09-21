59a545033803a2585193f54f3685c9d05fcb159f
Luciano Ramalho <luciano@ramalho.org>
2011-09-21 17:53:11 -0300

    demos palestra Unicamp

--- fundamentos/encoding/char_finder.py
+++ fundamentos/encoding/char_finder.py
@@ -3,12 +3,15 @@ import sys
 
 words = [s.lower() for s in sys.argv[1:]]
 
+#word = sys.argv[1].lower()
+
 count = 0
 for i in range(20, sys.maxunicode):
     car = unichr(i)
     descr = name(car, 'no-name')
     low_descr = descr.lower()
     if all(word in low_descr for word in words):
+    #if word in low_descr:  
         print u'{i:5d} {i:04x} {car:^5} {descr}'.format(**locals())
         count += 1
 
