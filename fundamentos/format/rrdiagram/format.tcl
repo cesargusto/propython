abe924673fe2f6a4284976077bd7f715abd11231
Luciano Ramalho <luciano@ramalho.org>
2011-09-24 01:44:19 -0300

    diagrama ferroviario da sintaxe de format

---
+++ fundamentos/format/rrdiagram/format.tcl
@@ -0,0 +1,51 @@
+set all_graphs {
+  marcacao {
+    line 
+      '{
+      {optx tcampot} 
+      {optx ! {or /s /r }}
+      {optx : tformatot}
+      '}
+  }
+  campo {
+    line {or tposicaot tnomet}
+    {optx
+      {loop
+        {or
+          {line . natributon}
+          {line [ nindicen ]}
+        }
+      }
+    }
+  }
+  formato {
+    line 
+    {optx
+      {optx tenchimentot}
+      talinhamentot
+    }
+    {optx tsinalt}
+    {optx #}
+    {optx tlargurat}
+    {optx ,}
+    {optx {line . tprecisaot}}
+    {optx ttipot}
+  }
+  formato2 {
+      stack 
+        {line
+          {optx
+            {optx tenchimentot}
+            talinhamentot
+          }
+          {optx tsinalt}
+          {optx #}
+        }
+        {line  
+          {optx tlargurat}
+          {optx ,}
+          {optx {line . tprecisaot}}
+          {optx ttipot}
+        }
+  }
+}
