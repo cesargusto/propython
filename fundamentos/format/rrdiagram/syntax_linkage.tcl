039728e89aefe2ccd93f6b4e07d0859757cf09be
Luciano Ramalho <luciano@ramalho.org>
2011-09-24 01:44:19 -0300

    diagrama ferroviario da sintaxe de format

---
+++ fundamentos/format/rrdiagram/syntax_linkage.tcl
@@ -0,0 +1,5 @@
+set syntax_linkage(campo) {{} {}}
+set syntax_linkage(formato) {{} {}}
+set syntax_linkage(formato2) {{} {}}
+set syntax_linkage(marcacao) {{} {}}
+set syntax_order {marcacao campo formato formato2}
