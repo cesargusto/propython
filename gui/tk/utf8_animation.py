3bb7adc36edffaf17e3fedc47046cbd8fe93a17a
Luciano Ramalho <luciano@ramalho.org>
2011-12-04 13:01:03 -0200

    exemplo com Jython

--- gui/tk/utf8_animation.py
+++ gui/tk/utf8_animation.py
@@ -4,13 +4,14 @@ import tkFont
 TOP_MARGIN = 32
 RIGHT_MARGIN = 10
 LINE_HEIGHT = 90
-FONT_SIZE = 55
+FONT_SIZE = 40
 
 root = Tk()
 
 bit_font = tkFont.Font(family='Courier', size=FONT_SIZE)
 
-canvas_width, canvas_height = root.winfo_screenwidth(), root.winfo_screenheight()
+#canvas_width, canvas_height = root.winfo_screenwidth(), root.winfo_screenheight()
+canvas_width, canvas_height = 1280, 720
 root.overrideredirect(1)
 root.geometry("%dx%d+0+0" % (canvas_width, canvas_height))
 
@@ -20,7 +21,7 @@ canvas.pack()
 bits = '0' * 8
 #bits = (' '+bits)*4
 
-code_bits = canvas.create_text(canvas_width-RIGHT_MARGIN, TOP_MARGIN, 
+code_bits = canvas.create_text(canvas_width-RIGHT_MARGIN, TOP_MARGIN,
     anchor='ne', text=bits, fill='white', font=bit_font)
 
 bits = (' '+bits)*4
@@ -29,9 +30,9 @@ utf8_bits = []
 bits = '0' * 8
 bits_width = bit_font.measure(bits+' ')
 for i in range(4):
-    utf8_bits.append(canvas.create_text(canvas_width-RIGHT_MARGIN-bits_width*i, 
-        TOP_MARGIN+LINE_HEIGHT, 
-        anchor='ne', text=bits, fill='dark gray', font=bit_font))   
+    utf8_bits.append(canvas.create_text(canvas_width-RIGHT_MARGIN-bits_width*i,
+        TOP_MARGIN+LINE_HEIGHT,
+        anchor='ne', text=bits, fill='dark gray', font=bit_font))
 
 START = 20
 counter = START
@@ -45,14 +46,14 @@ def animate():
         utf_codes = utf8(counter)
     except ValueError:
         counter = START
-    else:    
+    else:
         utf_codes.reverse()
         for i in range(4):
             try:
                 bits = '{0:08b}'.format(utf_codes[i])
             except IndexError:
                 bits = ' ' * 8
-            canvas.itemconfig(utf8_bits[i], text=bits)    
+            canvas.itemconfig(utf8_bits[i], text=bits)
     root.after(100, animate)
 
 clicked = False
