7e84b458589962e81b6c79c725b0af9e3e9317d0
Luciano Ramalho <luciano@ramalho.org>
2011-12-08 11:19:42 -0200

    pequenos ajustes nos exemplos do relogio

--- jython/relogio.py
+++ jython/relogio.py
@@ -1,28 +1,42 @@
-#!/usr/bin/env jython
-# coding: utf-8
-
 from javax.swing import *
 from java.awt import Font
 from time import strftime
 
 class Relogio(JFrame):
     def __init__(self):
-        JFrame.__init__(self, u'Relógio',
-            defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE,
-            size = (360,100))
-        self.mostrador = JLabel('HH:MM:SS',
+        JFrame.__init__(self, u'TicTac',
+            defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE)
+        self.mostrador = JLabel('00:00:00',
             font=Font('Sanserif',Font.BOLD, 70))
         self.contentPane.add(self.mostrador)
-        def tic(evento=None):
+        self.pack()
+        self.visible = True
+
+    def start(self):
+        def tic(evento):
             agora = strftime('%H:%M:%S')
             if self.mostrador.text != agora:
                 self.mostrador.text = agora
-        tic()
-        self.visible = True
         Timer(100, tic).start()
 
 if __name__=='__main__':
     rel = Relogio()
+    rel.start()
 
-#from java.awt import *
-#rel.mostrador.foreground = Color.RED
+DEMO = '''
+from relogio import Relogio
+rel = Relogio()
+rel.start()
+
+from java.awt import *
+rel.mostrador.foreground = Color.RED
+
+print rel.mostrador.text
+
+from time import sleep
+while True:
+    if rel.mostrador.text.endswith('0'):
+        print 'PING', rel.mostrador.text
+    sleep(1)
+     
+'''
--- jython/relogio0.py
+++ jython/relogio0.py
@@ -2,24 +2,23 @@
 # coding: utf-8
 
 from javax.swing import *
+from java.awt import Font
 from time import strftime
 
 frame = JFrame(u'Relógio',
-    defaultCloseOperation = JFrame.EXIT_ON_CLOSE,
-    size = (200,100),
-)
+    defaultCloseOperation = JFrame.EXIT_ON_CLOSE)
 
-formato = '<html><center><font size="7">%s</font></center></html>'
-rel = JLabel(formato)
+rel = JLabel('00:00:00',font=Font('Sanserif',Font.BOLD,70))
 frame.contentPane.add(rel)
+frame.pack()
 frame.visible = True
 
 def tic(evento):
-    agora = formato % strftime('%H:%M:%S')
+    agora = strftime('%H:%M:%S')
     if rel.text != agora:
         rel.text = agora
 
 Timer(100, tic).start()
 
 #from java.awt import *
-#rel.setForeground(Color.RED)
+#rel.foreground = Color.RED
