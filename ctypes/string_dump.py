1c560a85a7c0dfba24373e8afa5f5427ae167401
Luciano Ramalho <luciano@ramalho.org>
2012-06-08 02:52:43 -0300

    ainda tentando fazer dump de uma struct Py_Unicode

--- ctypes/string_dump.py
+++ ctypes/string_dump.py
@@ -1,5 +1,6 @@
 import ctypes
 from pprint import pprint
+import sys
 
 if hasattr(ctypes.pythonapi, 'Py_InitModule4'):
     Py_ssize_t = ctypes.c_int
@@ -8,7 +9,6 @@ elif hasattr(ctypes.pythonapi, 'Py_InitModule4_64'):
 else:
     raise TypeError("Cannot determine type of Py_ssize_t")
 
-print 'sizeof(Py_ssize_t) = %s bytes' % ctypes.sizeof(Py_ssize_t) 
 
 '''
 /* from cpython/Include/object.h */
@@ -26,8 +26,6 @@ PyObject._fields_ = [('ob_refcnt', Py_ssize_t),
                      ('ob_type', ctypes.POINTER(PyObject))] # recursive definition
 
 
-print 'sizeof(PyObject) = %s bytes' % ctypes.sizeof(PyObject) 
-
 
 '''
 /* from cpython/Include/intobject.h */
@@ -40,7 +38,9 @@ typedef struct {
 class PyIntObject(PyObject):
     _fields_ = [('ob_ival', ctypes.c_long)]
 
-print 'sizeof(PyIntObject) = %s bytes' % ctypes.sizeof(PyIntObject) 
+# print 'sizeof(Py_ssize_t) = %s bytes' % ctypes.sizeof(Py_ssize_t) 
+# print 'sizeof(PyObject) = %s bytes' % ctypes.sizeof(PyObject) 
+# print 'sizeof(PyIntObject) = %s bytes' % ctypes.sizeof(PyIntObject) 
 
 '''
 /* from cpython/Include/object.h */
@@ -68,35 +68,76 @@ typedef struct {
 } PyStringObject;
 '''
 
-class PyStringObject(PyObject):
-    _fields_ = [('ob_size', Py_ssize_t),
+def customPyStringObject(size):
+    class PyStringObject(PyObject):
+        _fields_ = [('ob_size', Py_ssize_t),
+                    ('ob_shash', ctypes.c_long),
+                    ('ob_sstate', ctypes.c_int),
+                    ('ob_sval', ctypes.c_char * size),
+                   ]
+    return PyStringObject
+    
+'''
+typedef struct {
+    PyObject_HEAD
+    Py_ssize_t length;          /* Length of raw Unicode data in buffer */
+    Py_UNICODE *str;            /* Raw Unicode buffer */
+    long hash;                  /* Hash value; -1 if not set */
+    PyObject *defenc;           /* (Default) Encoded version as Python
+                                   string, or NULL; this is used for
+                                   implementing the buffer protocol */
+} PyUnicodeObject;
+'''
+
+class PyUnicodeObject(PyObject):
+    _fields_ = [('ob_length', Py_ssize_t),
+                ('ob_str', ctypes.c_wchar_p),
                 ('ob_shash', ctypes.c_long),
-                ('ob_sstate', ctypes.c_int),
-                ('ob_sval', ctypes.POINTER(ctypes.c_char)),
                ]
 
-print 'sizeof(PyStringObject) = %s bytes' % ctypes.sizeof(PyStringObject) 
-
+HIDE = {'ob_refcnt', 'ob_shash', 'ob_type'}
 def show_attrs(obj):
     for name in dir(obj):
-        if name.startswith('ob_'):
+        if name.startswith('ob_') and name not in HIDE:
             val = getattr(obj, name)
             print '{0:10} = {1!r}'.format(name,val)
 
-print '*' * 20, 'n = 42'
-n = 42
-obj_n = PyIntObject.from_address(id(n))
-show_attrs(obj_n)
+# print '*' * 20, 'n = 42'
+# n = 42
+# obj_n = PyIntObject.from_address(id(n))
+# show_attrs(obj_n)
 
-print '*' * 20, "s0 = ''"
-s0 = ''
-obj_s0 = PyStringObject.from_address(id(s0))
-show_attrs(obj_s0)
+# print '*' * 20, "s0 = ''"
+# s0 = ''
+# obj_s0 = customPyString(len(s0)).from_address(id(s0))
+# show_attrs(obj_s0)
 
-print '*' * 20, "s1 = 'ABC'"
 s1 = 'ABC'
-obj_s1 = PyStringObject.from_address(id(s1))
+print '*' * 20, "s1 = %r" % s1
+obj_s1 = customPyStringObject(len(s1)).from_address(id(s1))
+print obj_s1.ob_sval
 show_attrs(obj_s1)
 
-print obj_s1.ob_sval.contents
+u1 = u'A'
+print '*' * 20, "u1 = %r" % u1
+obj_u1 = PyUnicodeObject.from_address(id(u1))
+print obj_u1.ob_str
+show_attrs(obj_u1)
 
+u1 = u'BA'
+print '*' * 20, "u1 = %r" % u1
+obj_u1 = PyUnicodeObject.from_address(id(u1))
+print obj_u1.ob_str
+show_attrs(obj_u1)
+
+u1 = u'ABZ'
+print '*' * 20, "u1 = %r" % u1
+obj_u1 = PyUnicodeObject.from_address(id(u1))
+print obj_u1.ob_str
+show_attrs(obj_u1)
+
+u1 = u'ABYZCD'
+print '*' * 20, "u1 = %r" % u1
+obj_u1 = PyUnicodeObject.from_address(id(u1))
+print obj_u1.ob_str
+show_attrs(obj_u1)
