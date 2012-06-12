0da898c79fe8c8460c5f0e672d5969adf6bb4e59
Luciano Ramalho <luciano@ramalho.org>
2012-06-11 21:42:32 -0300

    animacao com caracteres coloridos

--- gui/tk/codepages_anim.py
+++ gui/tk/codepages_anim.py
@@ -15,19 +15,18 @@ ESTILO_ROTULO = dict(anchor='center', font='courier 24', fill='gray')
 ESTILO_CAR = dict(anchor='center', font='times 32', fill='black')        
 ESTILO_LEGENDA = dict(anchor='w', font='helvetica 32', fill='black')
 
-CODEPAGES = ['ASCII', 'cp437', 'cp850', 'MacRoman', 
-'ISO-8859-1', 'ISO-8859-15', 'Windows-1252', 'ISO-8859-13', 'ISO-8859-2', 'ISO-8859-3', 'ISO-8859-9', 'ISO-8859-4', 
-'ISO-8859-7', 'ISO-8859-8', 
-# 'ISO-8859-10', 'ISO-8859-6-E', 'ISO-8859-6-I', 'ISO-8859-8-E', 'ISO-8859-8-I',
-# 'ISO-8859-14', 'ISO-8859-16', 'Shift_JIS', 'Windows-31J'
- 
-'ISO-8859-5', 'Windows-1251', 'KOI8-R', 'cp437', ]
+CODEPAGES = ['ASCII', 
+    'cp437', 'cp850', 'MacRoman', 'ISO-8859-1', 
+    'ISO-8859-15', 'Windows-1252', 'ISO-8859-2',  'ISO-8859-7', 
+    'ISO-8859-8', 'ISO-8859-5', 'Windows-1251', 'KOI8-R', 'cp437', ]
+
+COLORS = ['blue', '#700', '#070', 'orange']
 
 class Glifo(object):
     ativos = {}
     movendo = {}
 
-    def __init__(self, canvas, unicar, x, y):
+    def __init__(self, canvas, unicar, x, y, color='black'):
         self.canvas = canvas
         self.unicar = unicar
         self.x = x
@@ -35,6 +34,7 @@ class Glifo(object):
         self.vx = self.vy = self.ax = self.ay = 0
         self.x_dest = self.y_dest = None
         Glifo.ativos[self.unicar] = self
+        ESTILO_CAR['fill'] = color
         self.handle = self.canvas.create_text(x, y, 
                         text=unicar, **ESTILO_CAR)
 
@@ -67,30 +67,39 @@ class Glifo(object):
         self.y = self.y_dest
         self.vx = self.vy = self.ax = self.ay = 0
         del Glifo.movendo[self.unicar]
+        if hasattr(self, 'saindo') and self.saindo:
+            del Glifo.ativos[self.unicar]    
+            self.canvas.delete(self.handle)
 
-    @classmethod
-    def remover(cls, unicar):
-        glifo = cls.ativos.pop(unicar)    
-        glifo.canvas.delete(glifo.handle)
-        if unicar in Glifo.movendo: del Glifo.movendo[unicar]    
+    def sair(self):
+        # y = 13*CEL # saindo todos pelo mesmo y
+        y = self.canvas.coords(self.handle)[1]  # mantendo o y
+        self.partir(24*CEL, y)
+        self.saindo = True
 
-class Janela(Frame):
+class Janela(Tk):
     def __init__(self, raiz):
-        Frame.__init__(self, raiz)
+        Tk.__init__(self, raiz)
         self.canvas = Canvas(raiz, width=CEL*17+LATERAL, height=CEL*17)
         self.cels = [[0]*16 for i in range(16)]
+        self.id_pg = 0 # ASCII
         self.canvas.pack()
         self.desenhar_base()
         self.canvas.bind('<Double-Button-1>', lambda e: sys.exit())
         self.bind_all('<Left>', lambda e: self.desenhar_tabela(-1))
         self.bind_all('<Right>', lambda e: self.desenhar_tabela(1))
-        self.id_pg = 0 # ASCII
-        raiz.geometry('+1800+300')
+        self.geometry('+1800+300')
 
     def mudar_legenda(self, texto):
         if hasattr(self, 'legenda'):
             self.canvas.delete(self.legenda)
+        ESTILO_LEGENDA['fill'] = self.cor()
         self.legenda = self.canvas.create_text(17.5*CEL, 1.5*CEL, text=texto, **ESTILO_LEGENDA)
+
+    def cor(self):
+        if self.id_pg == 0: # ASCII
+            return 'black'
+        return COLORS[(self.id_pg-1)%len(COLORS)]
 
     def desenhar_tabela(self, muda_cod):
         self.id_pg_anterior = self.id_pg
@@ -108,14 +117,17 @@ class Janela(Frame):
                     unicar = chr(i*16+j).decode(encoding, 'ignore')
                     if not unicar:
                         continue
+                    if i == 8 and j == 5 and unicar == u'\x85':
+                        continue # ignorar gremlim na forma de Á dentro de um quadrado
                     glifo = Glifo.ativos.get(unicar)
                     if glifo is None:
-                        glifo = Glifo(self.canvas, unicar, 17.5*CEL, 1.5*CEL)
+                        glifo = Glifo(self.canvas, unicar, 17.5*CEL, 1.5*CEL, color=self.cor())
                     else:
                         unicars_sobrando.remove(unicar)
                     glifo.partir((j+1.5)*CEL, (i+1.5)*CEL)
         for unicar in unicars_sobrando:
-            Glifo.remover(unicar)
+            glifo = Glifo.ativos.get(unicar)
+            glifo.sair()
         self.atualizar()
 
     def atualizar(self):
@@ -123,11 +135,18 @@ class Janela(Frame):
         if Glifo.movendo:
             for unicar, glifo in Glifo.movendo.items():
                 glifo.mover()
-            self.after(50, self.atualizar)
+            self.after(40, self.atualizar)
+        else:
+            # limpar rastros
+            remendo = self.canvas.create_rectangle(CEL*17.1, 0, CEL*17+LATERAL, CEL*17, fill="white", outline='white')
+            self.canvas.tag_lower(remendo)
+            self.mudar_legenda(CODEPAGES[self.id_pg])
 
     def desenhar_base(self):
         c = self.canvas
         self.mudar_legenda('ASCII')
+        ESTILO_CAR['fill'] = 'black'
+
         for i in range(17):
             # linhas
             c.create_line(CEL, (i+1)*CEL, 17*CEL, (i+1)*CEL)
@@ -147,5 +166,5 @@ class Janela(Frame):
 
 
 
-raiz = Tk()
-Janela(raiz).mainloop()
+janela = Janela(None)
+janela.mainloop()
